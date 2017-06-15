5601b9dbdd2df0990cf0501090d547f8c240425e
itthesunny <mertzorlularmert@gmail.com>
2017-06-15 07:01:27 -0400

    Create AveragePopulationofEachContinent.sql

--- AveragePopulationofEachContinent.sql
+++ AveragePopulationofEachContinent.sql
@@ -1,3 +1,3 @@
-/* https://www.hackerrank.com/challenges/average-population-of-each-continent?h_r=next-challenge&h_v=zen
+-- https://www.hackerrank.com/challenges/average-population-of-each-continent?h_r=next-challenge&h_v=zen
 SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION)) FROM COUNTRY 
 INNER JOIN CITY ON COUNTRY.CODE = CITY.COUNTRYCODE GROUP BY country.continent;
